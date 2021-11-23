b90127ee840656386874bd17e3c7aaa18b40d31f
chxtio <chxtio@gmail.com>
2021-11-22 22:20:52 -0800

    Enable network reqeusts and create AsyncHttpClient

--- app/src/main/java/com/bmicalculator/MainActivity.java
+++ app/src/main/java/com/bmicalculator/MainActivity.java
@@ -3,12 +3,34 @@ package com.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String URL = "http://webstrar99.fulton.asu.edu/page3/Service1.svc/calculateBMI?height=60&weight=156";
+    public static  final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+            }
+
+            @Override
+            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/bmicalculator/Models/bmi.java
@@ -0,0 +1,11 @@
+package com.bmicalculator.Models;
+
+public class bmi {
+    double b;
+    String risk;
+    String[] more = {"https://www.cdc.gov/healthyweight/assessing/bmi/index.html", "https://www.nhlbi.nih.gov/health/educational/lose_wt/index.htm", "https://www.ucsfhealth.org/education/body_mass_index_tool/"};
+
+    public bmi() {
+
+    }
+}
