804e29476f00de449c6caf64e6f82d8ef232b4be
chxtio <chxtio@gmail.com>
2021-11-23 00:37:18 -0800

    Allow user to input fields and query BMI API

--- app/src/main/java/com/bmicalculator/MainActivity.java
+++ app/src/main/java/com/bmicalculator/MainActivity.java
@@ -4,17 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.bmicalculator.Models.bmi;
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String URL = "http://webstrar99.fulton.asu.edu/page3/Service1.svc/calculateBMI?height=60&weight=156";
+    private bmi BMI;
+    private Button bmi_button;
+    private Button label_button;
+    private Button message_button;
+    EditText h;
+    EditText w;
+    int height;
+    int weight;
+
+//    public static final String URL = "http://webstrar99.fulton.asu.edu/page3/Service1.svc/calculateBMI?height=60&weight=156";
+    public String URL;
     public static  final String TAG = "MainActivity";
 
     @Override
@@ -22,18 +37,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(URL, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int i, Headers headers, JSON json) {
-                Log.d(TAG, "onSuccess");
-                JSONObject jsonObject = json.jsonObject;
-                Log.i(TAG, "jsonObject: " + jsonObject);
-            }
+        h = findViewById(R.id.height);
+        w = findViewById(R.id.weight);
 
+        // Assign button IDs to objects
+        bmi_button = findViewById(R.id.button_bmi);
+        label_button = findViewById(R.id.button_label);
+        message_button = findViewById(R.id.button_message);
+
+        bmi_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
-                Log.d(TAG, "onFailure");
+            public void onClick(View v) {
+                try {
+                    height = Integer.parseInt(h.getText().toString());
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+//                    return;
+                    height = 0; //default fallback value
+                }
+
+                try {
+                    weight = Integer.parseInt(w.getText().toString());
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+//                    return;
+                    weight = 0; //default fallback value
+                }
+
+                URL =  "http://webstrar99.fulton.asu.edu/page3/Service1.svc/calculateBMI?height=" + height + "&weight=" + weight;
+
+                AsyncHttpClient client = new AsyncHttpClient();
+                client.get(URL, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int i, Headers headers, JSON json) {
+                        Log.d(TAG, "onSuccess");
+                        JSONObject jsonObject = json.jsonObject;
+                        Log.i(TAG, "jsonObject: " + jsonObject);
+                        try {
+                            BMI = new bmi(jsonObject);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                        Log.d(TAG, "onFailure");
+                        Log.d(TAG, "URL: " + URL);
+                    }
+                });
             }
         });
     }
